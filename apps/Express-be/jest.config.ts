263be1e8b5a41b4a3750588db493f75ea0c8d979
Robin Eberle <robineb174@gmx.de>
2025-09-06 12:15:50 +0200

    feat(Webapp): initialize new React application with Vite, TypeScript, and ESLint
    - Added main entry point for the application (main.tsx).
    - Created initial styles file (styles.scss).
    - Configured TypeScript settings (tsconfig.app.json, tsconfig.json).
    - Set up Vite configuration for development and build processes (vite.config.ts).
    - Established ESLint configuration for code quality and consistency (eslint.config.mjs).
    - Added Jest configuration for testing (jest.config.ts, jest.preset.js).
    - Included a placeholder for assets management (.gitkeep).
    - Created an empty error log file for tracking issues (error.log).

---
+++ apps/Express-be/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'Express-be',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/Express-be',
+};
