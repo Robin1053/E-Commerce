7aab8d9fd6a7e4fc34ba08c4eea8ed80f0e411a0
Robin1053 <robineb174@gmx.de>
2025-10-24 21:09:31 +0200

    Initial commit

---
+++ apps/Frontend/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@e-commerce/Frontend" />
+    </div>
+  );
+}
+
+export default App;
